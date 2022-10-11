4d896a0d4d322a6e09ccb6ae2c8ac944f082c7fa
ps195415 <ps195415@summacollege.nl>
2022-10-11 14:58:34 +0200

    react js commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Login from "./pages/login/Login";
+import New from "./pages/new/New";
+import Single from "./pages/single/single";
+import { 
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home/>} />
+            <Route path="login" element={<Login />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div> 
+  );
+}
+
+export default App;
---
+++ src/components/widget/Widget.jsx
@@ -0,0 +1,26 @@
+import "./widget.scss";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
+
+import React from "react";
+
+const Widget = () => {
+
+  return (
+    <div className="widget">
+      <div className="left">
+      </div>
+      <div className="right">
+        <div className="percentage positive">
+
+        </div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Widget;
